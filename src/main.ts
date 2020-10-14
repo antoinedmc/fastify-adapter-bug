476829bf7f7e4bfe619a7733e949233b14042b38
Antoine Demon-Chaine <a.demonchaine@gmail.com>
2020-10-14 11:10:15 +0200

    add setGlobalPrefix

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,15 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { FastifyAdapter } from '@nestjs/platform-fastify';
+import { Logger } from 'nestjs-pino';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const fastifyAdapter = new FastifyAdapter();
+  const app = await NestFactory.create(AppModule, fastifyAdapter, {
+    logger: false,
+  });
+  app.useLogger(app.get(Logger));
+  app.setGlobalPrefix('v1');
   await app.listen(3000);
 }
 bootstrap();
